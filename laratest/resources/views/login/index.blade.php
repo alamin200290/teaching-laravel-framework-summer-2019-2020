54db036e1c5c8cc687922efdefb13ef5129ea74a
sarwar_sikder <amin.se.aiub@gmail.com>
2020-08-20 13:04:53 +0600

    laravel startup

---
+++ laratest/resources/views/login/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+	
+	<h1>login</h1>
+	
+	<form method="post">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
